ad4f23c1704b08b8aafc0115dbf5bc9556d1caf9
Nerses Safaryan <nersessafaran341@gmail.com>
2025-10-27 11:13:17 +0400

    27.10.2025.4

--- app/meal/[id]/page.tsx
+++ app/meal/[id]/page.tsx
@@ -1,32 +1,32 @@
-import { Meal } from "@/components/content/Meal";
+import { Meal } from '@/components/content/Meal';
+import React from 'react'
 
-type MealType = {
+
+type Params = {
+  params: Promise<{ id: string }>
+}
+
+type Meal = {
   idMeal: string;
   strMeal: string;
-  strMealAlternate?: string | null;
+  strMealAlternate: string | null,
   strCategory: string;
   strArea: string;
   strInstructions: string;
   strMealThumb: string;
   strYoutube: string;
-  [key: string]: string | null | undefined;
-};
-
-type MealResponse = {
-  meals: MealType[];
-};
-
-interface PageProps {
-  params: { id: string };
+  [key: string]: string | number | null;
 }
 
-export default async function Page({ params }: PageProps) {
-  const { id } = params;
+type MealResponse = {
+  meals: Meal[]
+}
 
-  const res = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
-  );
-  if (!res.ok) throw new Error("Failed to fetch meal data");
+export default async function page({ params }: Params) {
+
+  const { id } = await params;
+  const res = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+  if (!res.ok) throw new Error(`Failed to fetch meal with id ${id}`)
 
   const data: MealResponse = await res.json();
   const meal = data.meals[0];
@@ -35,5 +35,5 @@ export default async function Page({ params }: PageProps) {
     <div>
       <Meal meal={meal} />
     </div>
-  );
+  )
 }
